40deff92fd3c567099fe0ba8e322f4be49943a44
Antonin Boulard <aboulard@localhost.localdomain>
2020-04-06 18:35:50 +0200

    [Removed] Useless print removed

--- src/InitShell.cpp
+++ src/InitShell.cpp
@@ -14,17 +14,7 @@ plazza::Shell::Shell(const std::string &prompt)
 
 void plazza::Shell::printIntroducing()
 {
-    std::cout << "Hi ! Welcome in our Restaurant !" << std::endl;
-    std::cout << "How many people are you ? : ";
-    while (!std::atoi(this->prompt.c_str())) {
-        if (!std::getline(std::cin, this->prompt))
-            return;
-        if (!std::atoi(this->prompt.c_str())) {
-            std::cout << "Please write a number !" << std::endl;
-            std::cout << "How many people are you ? : ";
-        }
-    }
-    std::cout << "\nOh great ! Please sit here !" << std::endl;
+    std::cout << "Hi ! Welcome to our Restaurant !" << std::endl;
     std::cout << "What do you want ?" << std::endl;
 }
 
