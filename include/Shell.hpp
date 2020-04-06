0be7cbb178ebaead7d5ec6f45c58dca8ac5f5232
Auguste Alexandre <Auguste0904@DESKTOP-2231JVC-1.home>
2020-04-06 13:47:10 +0200

    [Added] Enums added

--- include/Shell.hpp
+++ include/Shell.hpp
@@ -16,6 +16,23 @@
 
 namespace plazza
 {
+    enum PizzaType
+    {
+        Regina = 1,
+        Margarita = 2,
+        Americana = 4,
+        Fantasia = 8
+    };
+
+    enum PizzaSize
+    {
+        S = 1,
+        M = 2,
+        L = 4,
+        XL = 8,
+        XXL = 16
+    };
+
     class Shell
     {
         public:
